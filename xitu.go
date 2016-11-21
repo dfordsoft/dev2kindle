8d89ce29b1788fb262af0a37c83342c42eda515d
Fan Yang <missdeer@dfordsoft.com>
2016-11-21 10:03:25 +0800

    (*)get xitu articles via its rss feed

--- xitu.go
+++ xitu.go
@@ -1,8 +1,18 @@
 package main
 
+import "github.com/mmcdole/gofeed"
+
+const (
+	feedURL = "http://gold.xitu.io/rss"
+)
+
 type Xitu struct {
 }
 
 func (x *Xitu) Fetch(link chan string) {
-
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL(feedURL)
+	for _, item := range feed.Items {
+		link <- item.Link
+	}
 }
