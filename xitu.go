1ecbe82f6a9dfa70ae1267725307ceee00376676
Fan Yang <missdeer@dfordsoft.com>
2016-11-21 11:03:49 +0800

    (*)error handling

--- xitu.go
+++ xitu.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/mmcdole/gofeed"
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+)
 
 const (
 	feedURL = "http://gold.xitu.io/rss"
@@ -11,7 +15,11 @@ type Xitu struct {
 
 func (x *Xitu) Fetch(link chan string) {
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL(feedURL)
+	feed, err := fp.ParseURL(feedURL)
+	if err != nil {
+		fmt.Println("parsing feed URL failed", err)
+		return
+	}
 	for _, item := range feed.Items {
 		link <- item.Link
 	}
